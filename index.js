3e0fdfe579bf702a892e68c98599fa6933312c92
Joaquin Vargas <joaquin.vargas90@gmail.com>
2019-11-05 20:00:53 -0600

    1.2

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
-    function renderMovies(movieArray) {
-      var movieHTML = movieArray.map(function (currentMovie){
-        return `
-            <div class="movie">
-              <div class="card">
-                      <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
-                      <div class="card-body">
-                          <h5 class="card-title"><a href="https://www.imdb.com/title/${currentMovie.imdbID}/">${currentMovie.Title}</a> <span class="badge badge-secondary">${currentMovie.Year}</span></h5>
-                          <p class="card-text">IMDB ID: ${currentMovie.imdbID}</p>
-                          <a href="#" class="btn btn-primary" onclick="saveToWatchlist(${currentMovie.imdbID})">Add Movie</button></a>
-                      </div>
-              </div>
-            </div>
-            `;
-      });
+
+  function renderMovies(movieArray) {
+    var movieHTML = movieArray.map(function (currentMovie){
+      return `
+          <div class="movie">
+							<div class="card" style="width: 18rem;">
+									<img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
+									<div class="card-body">
+										<h5 class="card-title">${currentMovie.Title} <span class="badge badge-secondary">${currentMovie.Year}</span></h5>
+										<p class="card-text">IMDB ID: ${currentMovie.imdbID}</p>
+										<button onclick="saveToWatchlist('${currentMovie.imdbID}')" class="btn btn-primary">Go somewhere</button>
+									</div>
+							</div>
+          </div>
+          `;
+    });
+
+    return movieHTML.join('');
+  }
+
+  var content = document.getElementById('movies-container');
+  content.innerHTML = renderMovies(movieData);
   
-      return movieHTML.join('');
-    }  
-
-    document.getElementById('search-form').addEventListener('submit', function(e){
-        e.preventDefault();
-        var content = document.getElementById('movies-container');
-        content.innerHTML = renderMovies(movieData);
-    })
-
-  });
+});
 
 
-  function saveToWatchlist(imdbID){
-    var movie = movieData.find(function(currentMovie){
-      return currentMovie.imdbID = imdbID;
-    })
 
-    var watchlistJSON = localStorage.getItem("watchlist");
+function saveToWatchlist(imdbID) {
+  var movie = movieData.find(function (currentMovie) {
+      return currentMovie.imdbID == imdbID;
+  })
 
-    if(watchlist == null ){
-      watchlist =[];
-    }else{
-      var watchlist = JSON.parse(watchlistJSON);
-    }
-    watchlist.push(movie);
+  var watchlistJSON = localStorage.getItem("watchlist");
+  if (watchlistJSON === null) {
+    var watchlist = [];
+  } else{
+    var watchlist = JSON.parse(watchlistJSON);
+  }
 
-    watchlistJSON = JSON.stringify(watchlist)
-    localStorage.setItem("watchlist", watchlistJSON)
+  watchlist.push(movie);
 
-console.log(watchlist);
+  watchlistJSON = JSON.stringify(watchlist);
+  localStorage.setItem("watchlist", watchlistJSON);
+
+  console.log(watchlist);
 }
